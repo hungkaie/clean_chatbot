ae5bcedb56cc3d21b543a624d41b7b37344bb419
hungkaie <bestwisesthuman@gmail.com>
2024-02-05 22:58:58 +0800

    fix(default): fix missing api_key parameter in default.py

--- default.py
+++ default.py
@@ -3,6 +3,7 @@ from tools import tool_definitions, tool_functions
 
 
 class ChatBotDefault(BaseModel):
+    api_key: str = "sk-2"
     system_prompt: str = """你是一個聰明的 AI 助理，能簡潔有力的回答問題幫助大家。""",
     payload: dict = {
         'model': 'gpt-3.5-turbo-0125', #'gpt-4-1106-preview', #gpt-3.5-turbo-0125',
