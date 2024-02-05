161dbbe5bf223ccf169280a3230e60fd979749fb
hungkaie <bestwisesthuman@gmail.com>
2024-02-05 22:32:15 +0800

    feat(tools): add read_file function for txt and csv extension

--- tools.py
+++ tools.py
@@ -1,3 +1,5 @@
+import os
+import pandas as pd
 from googlesearch import search
 
 
@@ -8,14 +10,40 @@ def google_search(keyword):
     return content
 
 
-def write_file(filepath, content):
-    check = input(f"確定寫入檔案 {filepath}? (y/n) ")
-    if check == "y":
-        with open(filepath, "w", encoding="utf-8") as f:
-            f.write(content)
-        return f"成功寫入訊息至 {filepath}"
-    else:
+def read_txt_file(filepath: str):
+    with open(filepath, 'r', encoding="utf-8") as f:
+        return f.read()
+
+
+def read_csv_file(filepath: str):
+    return pd.read_csv(filepath).to_markdown()
+
+
+read_file_funs = {
+    '.txt': read_txt_file,
+    '.csv': read_csv_file
+}
+
+
+def write_file(filepath, content, need_check=True):
+    if need_check:
+        check = input(f"確定寫入檔案 {filepath}? (y/n) ")
+
+    if check != "y":
         return f"拒絕寫入訊息至 {filepath}"
+
+    with open(filepath, "w", encoding="utf-8") as f:
+        f.write(content)
+
+    return f"成功寫入訊息至 {filepath}"
+
+
+def read_file(filepath):
+    _, file_extension = os.path.splitext(filepath)
+    read_file_fun = read_file_funs.get(file_extension)
+    content = read_file_fun(filepath)
+
+    return content
 
 
 function_definitions = [
@@ -41,15 +69,29 @@ function_definitions = [
             "properties": {
                 "filepath": {
                     "type": "string",
-                    "description": "檔案路徑"
+                    "description": "要寫入的檔案路徑"
                 },
                 "content": {
                     "type": "string",
-                    "description": "檔案內容"
+                    "description": "要寫入的檔案內容"
                 }
             }
         },
         "required": ["filepath", "content"]
+    },
+    {
+        "name": "read_file",
+        "description": "讀取檔案",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "filepath": {
+                    "type": "string",
+                    "description": "要讀取的檔案路徑"
+                }
+            }
+        },
+        "required": ["filepath"]
     },
 ]
 
@@ -66,4 +108,5 @@ tool_definitions = [
 tool_functions = {
     "google_search": google_search,
     "write_file": write_file,
+    "read_file": read_file
 }
